411ef6097eb05aa63de7c5fa8f97471cc9041579
Hojeong <hjkim@firecop365.com>
2020-10-30 18:15:30 +0900

    First commit

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	bracketSpacing: false,
+	jsxBracketSameLine: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	semi: false,
+	useTabs: true,
+	tabWidth: 2,
+	printWidth: 80,
+	arrowParens: 'always',
+}
