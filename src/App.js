9522ea178389f94775fd3db07b7dc3d2ade64895
Hojeong <hjkim@firecop365.com>
2020-11-02 18:01:07 +0900

    Set module for SASS

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+import styles from './App.scss'
+
+const App = () => {
+	return (
+		<View style={styles.container}>
+			<Text className={styles.blue}>abc</Text>
+		</View>
+	)
+}
+
+export default App
