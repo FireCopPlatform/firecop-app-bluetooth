c77e76c1e02e49d168a172017670f18b95e68042
Yangwook Jeong <ywjeong@firecop365.com>
2020-11-04 14:14:31 +0900

    [add] apollo client, gql example queries

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import {View, Text} from 'react-native'
+import { ApolloProvider } from '@apollo/react-hooks'
+import client from './services'
 import styles from './App.scss'
 
 const App = () => {
 	return (
-		<View style={styles.container}>
-			<Text className={styles.blue}>abc</Text>
-		</View>
+		<ApolloProvider client={client}>
+			<View style={styles.container}>
+				<Text className={styles.blue}>abc</Text>
+			</View>
+		</ApolloProvider>
 	)
 }
 
---
+++ src/services/users.js
@@ -0,0 +1,19 @@
+import gql from 'graphql-tag'
+
+// getters
+export const GET_USERS = gql`
+  query {
+    users {
+      id name
+    }
+  }
+`
+
+// setters
+export const CREATE_USER = gql`
+  mutation($data: UserCreateInput!) {
+    createUser (data: $data) {
+      id name
+    }
+  }
+`
